28c50bde9850a48ef774c0521a7232817471946e
Alec Faithfull <alef@itu.dk>
2018-10-17 13:19:45 +0200

    Updated the copyright date

--- utilities/something-to-xes.py
+++ utilities/something-to-xes.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 # something-to-xes.py
-# Copyright © 2017 Alexander Faithfull
+# Copyright © 2017, 2018 Alexander Faithfull
 
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
